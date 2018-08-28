3bbeee541de6222e06635b255ed7581e876dca74
Hushino <frostshock@outlook.com>
2018-08-27 21:48:15 -0300

    new
    Signed-off-by: Hushino <frostshock@outlook.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AnimeComponent } from './anime/anime.component';
+import { EpisodesComponent } from './episodes/episodes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnimeComponent,
+    EpisodesComponent
   ],
   imports: [
     BrowserModule
